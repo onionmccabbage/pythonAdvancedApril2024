1e19612fa4870e2ebe6b813820e79f4d08073af2
onionmccabbage <onionmccabbage@live.com>
2024-04-29 10:43:47 +0100

    a simple class with slots, __str__ and @property

--- my_classes/my_concrete_class.py
+++ my_classes/my_concrete_class.py
@@ -1,18 +1,31 @@
 
 class Shape:
+    __slots__ = ('__s_name',) # a tuple of permitted properties
     def __init__(self, s_name):
-        if type(s_name) == str:
-            self.s_name = s_name
+        '''call the internal validation setter method'''
+        self.s_name = s_name # this will call the setter method
+    @property
+    def s_name(self): # getter method
+        return self.__s_name # this is called name mangling
+    @s_name.setter    # setter method
+    def s_name(self, new_name):
+        '''validate the property'''
+        if type(new_name)==str:
+            self.__s_name = new_name
         else:
-            self.s_name = 'unknown'
+            raise TypeError # or we can just set a sensible default
     def __str__(self):
         '''we override the built-in __str__ to specify how this class should print'''
         return f'This is the Shape class called {self.s_name}\n' # we may choose to include new line
     
 if __name__ == '__main__':
-    s = Shape(True) # we have an instance of our class (calls __init__)
-    s.s_name = -99
+    s = Shape('Oblong') # we have an instance of our class (calls __init__)
+    s.s_name = 'Square'  #this will call the internal validated setter method
+    # can we access the name-mangled property?
+    # s.__s_name = 'wibblywoo' # this fails due to slots
     s2 = Shape('Circle') 
     s3 = Shape('Triangle')
     print(s, s2, s3)
+    # we can still acces the mangled property name
+    print( s._Shape__s_name )
 
