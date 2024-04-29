f950e6b108b6c0f4ba26702cdc3c75d3bf0794e5
onionmccabbage <onionmccabbage@live.com>
2024-04-29 12:04:40 +0100

    using decorators

---
+++ my_classes/my_duck.py
@@ -0,0 +1,22 @@
+
+class Duck():
+    '''we may declare properties and methods that belong to the class (rather than to instances)'''
+    count = 0 # this property belongs to the duck class
+    def __init__(self, name):
+        self.name = name
+        Duck.count += 1 # we also have -= *= but not ++ or --
+
+    def __str__(self):
+        return f'This duck is called {self.name}'
+    @classmethod # the following method belongs to the class (rather than to an instance of the class)
+    def howMany(cls):
+        return f'There are {cls.count} ducks'
+    
+if __name__ == '__main__':
+    d1 = Duck('Howard')
+    d2 = Duck('Ethel')
+    d3 = Duck('Downy')
+    print(d1, d2, d3)
+    # how many ducks
+    print(Duck.count)
+    print(Duck.howMany())
