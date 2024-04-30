0afd3fcde84ed154a45014a1bb06b1aaeb9ee4a2
onionmccabbage <onionmccabbage@live.com>
2024-04-30 11:33:32 +0100

    updating db members

---
+++ using_db/update_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def customUpdate(w): # w will be an instance of a creeature (to be updated)
+    '''update the DB based on creature 'w' '''
+    q = int(float(w['count'])) # a bit of validation
+    a = w['creature']
+    conn = sqlite3.connect()
+    curs = conn.cursor()
+    # in SQL strings are in quotes, other values are NOT
+    st = f'''
+    UPDATE zoo
+    SET count={q}
+    WHERE creatue = "{a}"
+    '''
+
+
+if __name__ == '__main__':
+    # declare an updated creature
+    p = {'creature':'Penguin', 'count':305, 'cost':432.99}
+    customUpdate(p)
