9f9d2a5a6f8b945aaf9c6a6064b5e69140b8a9fc
onionmccabbage <onionmccabbage@live.com>
2024-04-30 09:35:51 +0100

    using type hints and 'inspect'

---
+++ inspecting/use_inspect.py
@@ -0,0 +1,19 @@
+import inspect
+ 
+def type_validator(func):
+    pars = inspect.signature(func)
+    def newFunc(*args, **kwargs):
+        arguments = pars.bind(*args, **kwargs).arguments
+        for arg, value in arguments.items():
+            if not isinstance(value, func.__annotations__[arg]):
+                raise TypeError('Incorrect type for ' + arg)
+        return func(*args, **kwargs)
+    return newFunc
+ 
+@type_validator
+def add(a: int, b: int):
+    return a + b
+ 
+add(2,3) # returns 5
+add('c', 4) # returns type-error
+# has context menu
