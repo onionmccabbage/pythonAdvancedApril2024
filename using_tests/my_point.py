71d0ed94f553f5b973129b9056f05dfd5b03ea82
onionmccabbage <onionmccabbage@live.com>
2024-05-01 15:52:40 +0100

    a point class

---
+++ using_tests/my_point.py
@@ -0,0 +1,26 @@
+class Point: # implicitly inherit from object
+    '''define a point in 2-d space represented by x and y as floating point values
+       Also define moveBy to allow changes to x and y (i.e. move the point)
+    '''
+    def __init__(self, x, y):
+        '''call setter methods for x and for y'''
+        self.x = x # call the property setter for x
+        self.y = y
+    @property
+    def x(self):
+        return self.__x # return the 'mangled' property value
+    @x.setter
+    def x(self, x):
+        if type(x)==float or type(x)==int:
+            self.__x = x # set a mangled property for x (only visible within the class)
+        else:
+            raise TypeError
+    @property
+    def y(self):
+        return self.__y # return the 'mangled' property value
+    @y.setter
+    def y(self, y):
+        if type(y)==float or type(y)==int:
+            self.__y = y # set a mangled property for x (only visible within the class)
+        else:
+            raise TypeError
