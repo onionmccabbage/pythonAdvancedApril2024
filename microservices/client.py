2b9afc553aba423c83c76c3bc7f280c128ee65ed
onionmccabbage <onionmccabbage@live.com>
2024-05-01 10:38:22 +0100

    simple socket client

--- microservices/client.py
+++ microservices/client.py
@@ -1,7 +1,14 @@
 import socket
 
-
-
+def client():
+    '''this client will make requests to our microservice over http'''
+    # match whatever our server is expecting
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port_t = ('localhost', 9874) 
+    sock.connect( port_t ) # we now connect to our server
+    #send a message to our server
+    message = 'hello' # or b'hello'
+    sock.send(message.encode()) # make sure we encode as bytes
 
 if __name__ == '__main__':
-    pass
+    client()
