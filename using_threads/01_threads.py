0847e2740f67b7b8b768afcbf1c7bbc875229a33
onionmccabbage <onionmccabbage@live.com>
2024-05-02 09:58:29 +0100

    passing args to thread functions

--- using_threads/01_threads.py
+++ using_threads/01_threads.py
@@ -3,10 +3,10 @@ import time
 import random
 
 # any function may be invoked on a separate thread
-def myFn():
+def myFn(n):
     '''this is a normal function just like any other'''
     time.sleep( random.random()*2 )#  sleep for 0-2 sec
-    print(f'This is the function')
+    print(f'This is the function {n}')
 
 if __name__ == '__main__':
     # we may invoke the function in the normal manner (procedural)
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     # myFn()
     # myFn()
     # we may choose to incvoke any function in a separate thread
-    tA = Thread(target=myFn)
-    tB = Thread(target=myFn)
-    tC = Thread(target=myFn)
+    tA = Thread(target=myFn, args=('A',)) # one-member tuple (,)
+    tB = Thread(target=myFn, args=('B',))
+    tC = Thread(target=myFn, args=('C',))
     print('on the mainthread')
     tA.start() # at this point the function will start execution on a separate thread
     tB.start() # the threads run concurrently
