4bd97ba4136298af0802d0a1a12221ca89850218
onionmccabbage <onionmccabbage@live.com>
2024-05-02 09:54:32 +0100

    using threads to invoke functions

--- using_threads/01_threads.py
+++ using_threads/01_threads.py
@@ -1,3 +1,4 @@
+from threading import Thread
 import time
 import random
 
@@ -8,7 +9,20 @@ def myFn():
     print(f'This is the function')
 
 if __name__ == '__main__':
-    # we may invoke the function in the normal manner
-    myFn()
-    myFn()
-    myFn()
+    # we may invoke the function in the normal manner (procedural)
+    # myFn()
+    # myFn()
+    # myFn()
+    # we may choose to incvoke any function in a separate thread
+    tA = Thread(target=myFn)
+    tB = Thread(target=myFn)
+    tC = Thread(target=myFn)
+    print('on the mainthread')
+    tA.start() # at this point the function will start execution on a separate thread
+    tB.start() # the threads run concurrently
+    tC.start() # by now we have FOUR threads - the main one and these three extras
+    # by default our threads are not blocking the main thread
+    tA.join() # if we choose to join, then the main thread will wait until the other thread (re)joins
+    tB.join()
+    tC.join()
+    print('still on the main thread')
