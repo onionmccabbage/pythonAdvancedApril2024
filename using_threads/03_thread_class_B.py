7ff12e9159038de0e2ba0a5b81260d697fe4aeeb
onionmccabbage <onionmccabbage@live.com>
2024-05-02 10:55:47 +0100

    running a great number of concurrent threads

--- using_threads/03_thread_class_B.py
+++ using_threads/03_thread_class_B.py
@@ -1,5 +1,6 @@
 from threading import Thread
 import time
+import timeit # this is a more accurate library for measuring small time periods
 import random
 
 class MyClassB(): # we do not have to inherit from Thread
@@ -12,7 +13,7 @@ class MyClassB(): # we do not have to inherit from Thread
         '''__call__ lets us invoke this on a thread'''
         msg = ''
         for i in range(0,10): # on average this should take 0.5 sec
-            msg += f'{n} is sleeping\n'
+            msg += f'{n} is sleeping - '
             time.sleep(random.random()*0.1) # sleep for up to 1/10 sec
         print(msg, end='') # try to minimize I/O
 
@@ -21,15 +22,17 @@ def main():
     cA = MyClassB() # we have access to a new class instance
     print('on the main thread')
     thread_l = []
-    for _ in range(0,8):
+    for _ in range(0,32): # even up to 1024 threads is taking about 1 second
         thread_l.append(Thread(target=cA, args=(_,)))
     print(f'Main thread has spawned several child threads')
+    start = timeit.default_timer() #timeit attempts to ignore non-python stuff
     for item in thread_l:
         item.start()
     for item in thread_l:
         item.join()
-
-    print('still on the main thread')
+    end = timeit.default_timer()
+    # under 1 sec when concurrent (0.7sec for 8 other threads)
+    print(f'total time: {end-start}') # just over 4 seconds proedurally
 
 
 if __name__ == '__main__':
