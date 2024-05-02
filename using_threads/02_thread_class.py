a65af4d6a2f0770af67f5fd2bbfe740a70edf856
onionmccabbage <onionmccabbage@live.com>
2024-05-02 10:14:55 +0100

    using lots of threads

--- using_threads/02_thread_class.py
+++ using_threads/02_thread_class.py
@@ -4,9 +4,10 @@ import random
 
 class MyClass(Thread):
     '''Any class may be invoked as a thread, but it is easiest if the class inherists from Thread'''
-    def __init__(self, n):
+    def __init__(self, n, x):
         Thread.__init__(self) # we call the __init__ of the parent class
         self.n = n # we could use property get/set methods
+        self.x = x
     def run(self):
         '''When this class is invoked the run method will be executed'''
         for i in range(0,10): # on average this should take 0.5 sec
@@ -16,8 +17,22 @@ class MyClass(Thread):
 def main():
     '''we can invoke our class as a thread'''
     print('on the main thread')
-    tA = MyClass('A') # we have access to a new thread
+    tA = MyClass('A', True) # we have access to a new thread
+    tB = MyClass('A', []) 
+    tC = MyClass('A', (3,2,1))
+    tD = MyClass('A', {3,4,5,6,7}) 
+    tE = MyClass('A', tA) 
+
     tA.start()
+    tB.start()
+    tC.start()
+    tD.start()
+    tE.start()
+    tA.join() # this causes the main thread to wait
+    tB.join()
+    tC.join()
+    tD.join()
+    tE.join()
     print('still on the main thread')
 
 
