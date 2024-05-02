e6519e687711f8e2050f971442c174d0fe565a6f
onionmccabbage <onionmccabbage@live.com>
2024-05-02 11:50:28 +0100

    using locks

--- using_threads/04_thread_lock.py
+++ using_threads/04_thread_lock.py
@@ -1,20 +1,25 @@
 import threading
 
 counter = 1 # here is a global asset (could be some large resource we share)
+lock = threading.Lock()
 
 def workerA():
     '''this worker will acess the global counter to increment by 1 up to 10'''
     global counter
+    lock.acquire() #we have exclusive access to the lock
     while counter <10:
         counter += 1
-        print('Worker A incrememnts to {counter}')
+        print(f'Worker A incrememnts to {counter}')
+    lock.release()
 
 def workerB():
     '''this worker will acess the global counter to deccrement by 1 down to -10'''
     global counter
+    lock.acquire() #we have exclusive access to the lock
     while counter >-10:
         counter -= 1
-        print('Worker B decrememnts to {counter}')
+        print(f'Worker B decrememnts to {counter}')
+    lock.release()
 
 def main():
     '''invoke the workers as threads'''
